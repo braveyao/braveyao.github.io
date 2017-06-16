d58ee289ba760b986277ad07f1515067b39872f8
braveyao <braveyao@chromium.org>
2017-06-16 12:39:26 -0700

    add camera selection to resolution test

--- src/content/getusermedia/resolution/js/main.js
+++ src/content/getusermedia/resolution/js/main.js
@@ -10,12 +10,43 @@
 
 var dimensions = document.querySelector('#dimensions');
 var video = document.querySelector('video');
+var videoSelect = document.querySelector('select#videoSource');
+var selectors = [videoSelect];
 var stream;
 
 var vgaButton = document.querySelector('#vga');
 var qvgaButton = document.querySelector('#qvga');
 var hdButton = document.querySelector('#hd');
 var fullHdButton = document.querySelector('#full-hd');
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  var values = selectors.map(function(select) {
+    return select.value;
+  });
+  selectors.forEach(function(select) {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    }
+  }
+  selectors.forEach(function(select, selectorIndex) {
+    if (Array.prototype.slice.call(select.childNodes).some(function(n) {
+      return n.value === values[selectorIndex];
+    })) {
+      select.value = values[selectorIndex];
+    }
+  });
+}
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
 
 vgaButton.onclick = function() {
   getMedia(vgaConstraints);
@@ -71,6 +102,9 @@ function getMedia(constraints) {
     });
   }
 
+  var videoSource = videoSelect.value;
+  constraints.video.deviceId = videoSource ? {exact: videoSource} : undefined;
+
   navigator.mediaDevices.getUserMedia(constraints)
   .then(gotStream)
   .catch(function(e) {
@@ -78,4 +112,8 @@ function getMedia(constraints) {
     alert(message);
     console.log(message);
   });
+}
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
 }
